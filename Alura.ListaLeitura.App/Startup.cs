5c38eb2add990258d20fb0a51819486ca5c7bd3a
Hugo <hugo.lima.chaves@gmail.com>
2021-11-05 14:04:00 -0300

    invalidando caminhos que não são paths válidos e retornando apenas Lidos, Lendo e seraLido

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Alura.ListaLeitura.App
 {
@@ -12,6 +13,25 @@ namespace Alura.ListaLeitura.App
             app.Run(Roteamento);
         }
 
+        public Task Roteamento(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new  Dictionary<string, string>
+            {
+                {"/Livros/ParaLer", _repo.ParaLer.ToString() },
+                {"/Livros/Lendo", _repo.Lendo.ToString() },
+                {"/Livros/Lidos", _repo.Lidos.ToString()}
+
+            };
+
+            if( caminhosAtendidos.ContainsKey(context.Request.Path) )
+            {
+                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path.ToString()]);
+            }
+
+            return context.Response.WriteAsync("caminho inexistente");
+        }
+
         public Task LivrosParaLer(HttpContext context) //context é um objeto que tem todas as informações específicas sobre a requisição.
         {
 
@@ -19,9 +39,6 @@ namespace Alura.ListaLeitura.App
             return context.Response.WriteAsync(_repo.ParaLer.ToString()); // aqui eu vou escreve nas resposta alguma coisa (nesse contexto é uma lista de livros)
         }
 
-        public Task Roteamento(HttpContext context)
-        {
-            return context.Response.WriteAsync(context.Request.Path);
-        }
+       
     }
 }
