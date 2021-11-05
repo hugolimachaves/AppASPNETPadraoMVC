f2131702e6af2cd6733e130b767f80e2cc6d62df
Hugo <hugo.lima.chaves@gmail.com>
2021-11-05 13:44:09 -0300

    rederização echoa o nome da rota

---
+++ Alura.ListaLeitura.App/Program.cs
@@ -0,0 +1,32 @@
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace Alura.ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            IWebHost host = new WebHostBuilder()  // IWebhost: Hospedeiro Web. Para criar um objeto que implementa essa insterface, utilizamos um builder (WebHostBuilder)
+                .UseKestrel() // Vamos usar para esse host o servidor Kestrel
+                .UseStartup<Startup>() // indica qual é a classe de inicialização desse servidor
+                .Build();
+
+            host.Run();//bloqueia a execução das linhas abaixo até que o servido seja encerrado
+
+            ImprimeLista(_repo.ParaLer);
+            ImprimeLista(_repo.Lendo);
+            ImprimeLista(_repo.Lidos);
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+
+}
---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(Roteamento);
+        }
+
+        public Task LivrosParaLer(HttpContext context) //context é um objeto que tem todas as informações específicas sobre a requisição.
+        {
+
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString()); // aqui eu vou escreve nas resposta alguma coisa (nesse contexto é uma lista de livros)
+        }
+
+        public Task Roteamento(HttpContext context)
+        {
+            return context.Response.WriteAsync(context.Request.Path);
+        }
+    }
+}
