0e90b6d7bc6bd56d54441ce848c0012053af5369
Hugo <hugo.lima.chaves@gmail.com>
2021-11-24 12:07:36 -0300

    Separando HTML do código c#

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -3,16 +3,88 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Alura.ListaLeitura.App.Negocio;
+using System;
+using System.Linq;
+using System.IO;
 
 namespace Alura.ListaLeitura.App
 {
     public class Startup
     {
-
-        LivroRepositorioCSV _repo = new LivroRepositorioCSV();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting(); //serviço de roteamento do aspnet core
+        }
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(Roteamento);
+            var builder = new RouteBuilder(app);
+            builder.MapRoute("Livros/ParaLer", LivrosParaLer);
+            builder.MapRoute("Livros/Lendo", LivrosLendo);
+            builder.MapRoute("Livros/Lidos", LivrosLidos);
+            builder.MapRoute("Cadastro/NovoLivro/{nome}/{autor}", NovoLivroParaLer);
+            builder.MapRoute("Livros/Detalhes/{id:int}", ExibeDetalhes); // colocando a restrição no tipo de dado de id : {tipo}
+            builder.MapRoute("Cadastro/NovoLivro", ExibeFormulario);
+            builder.MapRoute("Cadastro/Incluir", ProcessaFormulario);
+            var rotas = builder.Build();
+
+            app.UseRouter(rotas);
+            //app.Run(Roteamento);
+        }
+
+        public Task ProcessaFormulario(HttpContext context)
+        {
+            var livro = new Livro()
+            {
+                Autor = context.Request.Query["autor"].First(),
+                Titulo = context.Request.Query["titulo"].First()
+            };
+            Console.WriteLine(livro.Titulo);
+            Console.WriteLine(livro.Autor);
+            var repo = new LivroRepositorioCSV();
+            repo.Incluir(livro);
+            return context.Response.WriteAsync("O livro foi adicionado com sucesso");
+
+
+        }
+
+        public Task ExibeFormulario(HttpContext context)
+        {
+            var html = CarregaArquivoHTML("formulario");
+            return context.Response.WriteAsync(html);
+        }
+
+        private string CarregaArquivoHTML(string nomeArquivo)
+        {
+        
+            var nomeCompletoArquivo = $"D:/Cursos/C#_web/Alura.ListaLeitura/Alura.ListaLeitura.App/HTML/{nomeArquivo}.html";
+            //Nota: importante usar o $ para interpolar string com variaveis
+            using( var arquivo = File.OpenText(nomeCompletoArquivo)) 
+            {
+                return arquivo.ReadToEnd();
+            }
+        }
+
+        public Task ExibeDetalhes(HttpContext context)
+        {
+            int id = Convert.ToInt32(context.GetRouteValue("id"));
+            var repo = new LivroRepositorioCSV();
+            var livro = repo.Todos.First(l => l.Id == id);
+            return context.Response.WriteAsync(livro.Detalhes());
+        }
+        public Task NovoLivroParaLer(HttpContext context)
+        {
+
+            var livro = new Livro()
+            {
+                Titulo = context.GetRouteValue("nome").ToString(), // ambas conversões de string são válidas!
+                Autor = Convert.ToString(context.GetRouteValue("autor"))  // ambas conversões de string são válidas!
+            };
+            var repo = new LivroRepositorioCSV();
+            repo.Incluir(livro);
+            return context.Response.WriteAsync("O livro foi adicionado com sucesso");
         }
 
         public Task Roteamento(HttpContext context)
@@ -35,6 +107,7 @@ namespace Alura.ListaLeitura.App
             return context.Response.WriteAsync("caminho inexistente");
         }
 
+        LivroRepositorioCSV _repo = new LivroRepositorioCSV();
         public Task LivrosParaLer(HttpContext context) //context é um objeto que tem todas as informações específicas sobre a requisição.
         {
 
@@ -50,7 +123,7 @@ namespace Alura.ListaLeitura.App
 
         public Task LivrosLidos(HttpContext context)
         {
-            return context.Response.WriteAsync(_repo.Lendo.ToString());
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
         }
 
        
